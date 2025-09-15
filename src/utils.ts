06187d690df00eb058a96d48d9ed2669e6d0d3b1
Joonas Palosuo <joonas.palosuo@gmail.com>
2025-09-15 19:38:28 +0000

    feat: style blog listing

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,10 @@
 export const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
-    arr.reduce((groups, item) => {
-        (groups[key(item)] ||= []).push(item);
-        return groups;
-    }, {} as Record<K, T[]>);
+    arr.reduce(
+        (groups, item) => {
+            (groups[key(item)] ||= []).push(item);
+            return groups;
+        },
+        {} as Record<K, T[]>,
+    );
 
+export const getDateString = (date: Date) => date.toISOString().slice(0, 10);
